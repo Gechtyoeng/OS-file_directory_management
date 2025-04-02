461811849ff36ee9298293aa8f7366a353ce0c09
veysean <veyseankem@gmail.com>
2025-04-02 16:13:29 +0700

    Add log function

--- list_details.sh
+++ list_details.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+source log.sh
 list_files() {
     read -p "Enter directory or file path: " path
     if [ -d "$path" ]; then
         ls -l "$path"
-        echo "$(date): Listed files in directory $path" >> script.log
+        log_action "Listed directories in $path"
     elif [ -f "$path" ]; then
         ls -l "$path"
-        echo "$(date): Listed file $path" >> script.log
+        log_action "Listed files in $path"
     else
         echo "Invalid path"
-        echo "$(date): Invalid path entered: $path" >> script.log
+        log_action "Invalid  $path" 
     fi
 }
---
+++ log.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+log_action() {
+    local message=$1
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> script.log
+}
+log_action
