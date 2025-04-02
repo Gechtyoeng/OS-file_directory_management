8c5eff1b123da2b6e0760fdafe9d4a80b1555c96
Chanalizain <inchanaliza@gmail.com>
2025-04-02 23:32:21 +0700

    updated disk_usage

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-#log file location
-LOG_FILE="script.log"
 
-#function to display disk usage
+#Source the script
+source ./log.sh
+
+#Function to display disk usage
 display_disk_usage() {
-	#ask user for directory input
 	read -p "Enter the directory path: " dir
 
-	#check if directory exists
-	if [ -d "$dir" ]; then 
+	if [ -d "$dir" ]; then
 		echo "Disk usage for: $dir"
 		du -sh "$dir"
-		
-		#log the action
-		echo "$(date +"%Y-%m-%d %H:%M:%S") - Checked disk usage of $dir" >> "$LOG_FILE"
-	else 
-		echo "Error: Directory does not exist!"
-		#log the error
-		echo "$(date +"%Y-%m-%d %H:%M:%S") - Error: Directory $dir does not exist" >> "$LOG_FILE"
+
+		#Log the action
+		log_action "Checked disk usage of $dir"
+	else
+		echo "Enter: Directory does not exist!"
+
+		#log_action error
+		log_action "Error: Directory $dir does not exist!"
 	fi
 }
+
+#Call the function automatically when the script run
+display_disk_usage
